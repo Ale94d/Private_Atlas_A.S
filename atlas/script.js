b97df1a43c4c0061a2ab99b8bc120971795713cd
Alejo <alejandro99castanedam@gmail.com>
2026-08-07 16:41:29 +0000

    pergamino

--- atlas/script.js
+++ atlas/script.js
@@ -9,3 +9,40 @@ if (backHome) {
     });
 
 }
+
+const mapContainer = document.getElementById("map-container");
+
+let isDragging = false;
+
+let startX = 0;
+let startY = 0;
+
+let translateX = 0;
+let translateY = 0;
+
+mapContainer.addEventListener("mousedown", (e) => {
+
+    isDragging = true;
+
+    startX = e.clientX - translateX;
+    startY = e.clientY - translateY;
+
+});
+
+document.addEventListener("mouseup", () => {
+
+    isDragging = false;
+
+});
+
+document.addEventListener("mousemove", (e) => {
+
+    if (!isDragging) return;
+
+    translateX = e.clientX - startX;
+    translateY = e.clientY - startY;
+
+    mapContainer.style.transform =
+        `translate(${translateX}px, ${translateY}px)`;
+
+});
