a9a2a2c27437c86033c665cc557252ee58eadb6d
Alejo <alejandro99castanedam@gmail.com>
2026-08-07 19:02:03 +0000

    Rediseño base del Atlas y preparacion del mapa

--- atlas/script.js
+++ atlas/script.js
@@ -1,48 +1,110 @@
-const backHome = document.querySelector(".back-btn");
-
-if (backHome) {
-
-    backHome.addEventListener("click", () => {
-
-        window.location.href = "../index.html";
-
-    });
-
-}
-
-const mapContainer = document.getElementById("#map-container");
+const mapContainer = document.getElementById("map-container");
 
 let isDragging = false;
 
 let startX = 0;
 let startY = 0;
 
-let translateX = 0;
-let translateY = 0;
+let currentX = 0;
+let currentY = 0;
 
-mapContainer.addEventListener("mousedown", (e) => {
+mapContainer.addEventListener("mousedown",(e)=>{
 
     isDragging = true;
 
-    startX = e.clientX - translateX;
-    startY = e.clientY - translateY;
+    startX = e.clientX - currentX;
+    startY = e.clientY - currentY;
+
+    mapContainer.style.cursor="grabbing";
 
 });
 
-document.addEventListener("mouseup", () => {
+document.addEventListener("mouseup",()=>{
 
     isDragging = false;
 
+    mapContainer.style.cursor="grab";
+
 });
 
-document.addEventListener("mousemove", (e) => {
+document.addEventListener("mousemove",(e)=>{
 
-    if (!isDragging) return;
+    if(!isDragging) return;
 
-    translateX = e.clientX - startX;
-    translateY = e.clientY - startY;
+    currentX = e.clientX - startX;
+    currentY = e.clientY - startY;
+
+    const limitX = 1200;
+    const limitY = 700;
+
+    currentX = Math.max(-limitX,Math.min(limitX,currentX));
+    currentY = Math.max(-limitY,Math.min(limitY,currentY));
 
     mapContainer.style.transform =
-        `translate(${translateX}px, ${translateY}px)`;
+    `translate(${currentX}px,${currentY}px) scale(${scale})`;
+
+});
+
+let scale = 1;
+
+document.addEventListener("wheel",(e)=>{
+
+    e.preventDefault();
+
+    if(e.deltaY<0){
+
+        scale+=0.08;
+
+    }else{
+
+        scale-=0.08;
+
+    }
+
+    scale=Math.max(.6,Math.min(scale,4));
+
+    mapContainer.style.transform=
+    `translate(${currentX}px,${currentY}px) scale(${scale})`;
+
+},{passive:false});
+
+window.addEventListener("load",()=>{
+
+    currentX = -900;
+    currentY = -350;
+
+    mapContainer.style.transform=
+    `translate(${currentX}px,${currentY}px) scale(${scale})`;
+
+});
+
+mapContainer.addEventListener("dblclick",()=>{
+
+    currentX = -900;
+    currentY = -350;
+    scale = 1;
+
+    mapContainer.style.transform =
+    `translate(${currentX}px,${currentY}px) scale(${scale})`;
+
+});
+
+const worldMap = document.getElementById("world-map");
+
+worldMap.addEventListener("load",()=>{
+
+    console.log("Mapa cargado correctamente.");
+
+});
+
+const memoryWindow =
+document.getElementById("country-memory");
+
+const closeMemory =
+document.getElementById("close-memory");
+
+closeMemory.addEventListener("click",()=>{
+
+    memoryWindow.style.display="none";
 
 });
